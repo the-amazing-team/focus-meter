9099e55231c224dd25ba3ab31ac7a6cd025fa909
dusklight00 <r.rahul.developer@gmail.com>
2024-01-06 22:56:15 +0530

    speech-to-text module converted to class

---
+++ audio_focus.py
@@ -0,0 +1,7 @@
+from services.offensive_language_detector.OffensiveLanguageDetector import (
+    OffensiveLanguageDetector,
+)
+from services.vocal_emotion.VocalEmotionDetection import VocalEmotionDetection
+
+language_detector = OffensiveLanguageDetector()
+vocal_detector = VocalEmotionDetection()
