76586b56f92d3ee6c6ff202aa50759f3713c6259
dusklight00 <r.rahul.developer@gmail.com>
2024-01-06 12:32:52 +0530

    get_emotional_focus function complete in emotion-detector

--- services/emotion/Engine/EmotionDetector.py
+++ services/emotion/Engine/EmotionDetector.py
@@ -1,29 +1,41 @@
 import cv2
 from fer import FER
-from Engine.FaceDetector import FaceDetector
+from services.emotion.Engine.FaceDetector import FaceDetector
+
 
 class EmotionDetector(FaceDetector):
     def __init__(self):
-        self.emotion_detector = FER(mtcnn = True)
+        self.emotion_detector = FER(mtcnn=True)
 
     def detect_emotion(self, image):
         emotion_array = self.emotion_detector.detect_emotions(image)
         emotion_array = emotion_array[0]["emotions"] if len(emotion_array) else None
-        max_emotion = max(emotion_array, key=emotion_array.get) if emotion_array is not None else None
+        max_emotion = (
+            max(emotion_array, key=emotion_array.get)
+            if emotion_array is not None
+            else None
+        )
         return max_emotion if max_emotion is not None else "neutral"
-    
+
     def label_frame_with_emotion(self, image):
         emotion = self.detect_emotion(image)
         emotion_image = self.get_emotion_image(emotion)
 
         labelled_image = self.overlay_face_box_indicator(
-            image = image,
-            emotion_image = emotion_image,
-            label_one = emotion,
+            image=image,
+            emotion_image=emotion_image,
+            label_one=emotion,
         )
 
         return labelled_image
-    
+
     def get_emotion_image(self, emotion_name):
         return cv2.imread(f"Emojis/{emotion_name}.png")
-    
+
+    def get_emotional_focus(self, image):
+        emotion = self.detect_emotion(image)
+
+        if emotion == "neutral" or emotion == "sad" or emotion == "fear":
+            return True
+
+        return False
--- visual_focus.py
+++ visual_focus.py
@@ -3,18 +3,19 @@ from services.drowsiness.DrowsinessDetector import DrowsinessDetector
 from services.headpose.HeadposeDetector import HeadposeDetector
 
 # from services.headpose import HeadposeDetector
-# from services.emotion.Engine import EmotionDetector
+from services.emotion.Engine.EmotionDetector import EmotionDetector
 
 drowsiness_detector = DrowsinessDetector()
 headpose_detector = HeadposeDetector()
-# emotion_detector = EmotionDetector()
+emotion_detector = EmotionDetector()
 
-sample_image = cv2.imread("sample3.jpg")
+sample_image = cv2.imread("sample7.jpg")
 
 # cv2.imshow("photo", sample_image)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
 # result = drowsiness_detector.get_drowsiness(sample_image)
-unfocusness, image = headpose_detector.get_unfocus_headpose_percentage(sample_image)
-print(unfocusness)
+# unfocusness, image = headpose_detector.get_unfocus_headpose_percentage(sample_image)
+emotion = emotion_detector.get_emotional_focus(sample_image)
+print(emotion)
